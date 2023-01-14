16bc9fe1f8de195e71fd403f1f37cffa21c1aadc
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2023-01-14 21:54:29 +0000

    done now

---
+++ js/index.js
@@ -0,0 +1,71 @@
+// Search Bar & Toggle
+//open to bar button 
+$('#toggle-search').on('click', function() {
+    $('#searchBar').fadeIn("slow");
+  });
+
+//close top bar 
+
+$('.fa-xmark ').on('click', function() {
+    $('#searchBar').fadeOut("slow");
+  });
+
+
+//navbar toggler
+function hideFunction(x) {
+    
+    $('.fa-toggle-on').hide();
+    $('.fa-toggle-off').show();
+
+    // x.classList.toggle("fa-solid fa-toggle-off");
+  }
+  function showFunction(x) {
+    
+    $('.fa-toggle-on').show();
+    $('.fa-toggle-off').hide();
+
+    // x.classList.toggle("fa-solid fa-toggle-off");
+  }
+  
+
+
+
+
+
+//   captionshow in multi carousel section
+
+// $(".bg-image").(()=> {
+//     $(".captionShow").show()
+// })
+
+// $('.bg-image').hover(mouseEnter, mouseLeave);
+// function mouseEnter() {
+//    $(".captionShow").show()
+// };
+// function mouseLeave() {
+//      $(".captionShow").hide("slow")
+// };
+
+
+
+
+// button that takes user to top of the screen
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+ } else {
+    mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
